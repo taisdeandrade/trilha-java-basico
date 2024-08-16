d55aa7c14c86c61f2ff3a70643d976ebcfd0f71a
TaisAndrade <taismdeandrade@gmail.com>
2024-08-16 10:11:38 -0300

    Subindo projeto

---
+++ desafio-controle-fluxo/src/Contador.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro numero: ");
+        int numeroUm = terminal.nextInt();
+        System.out.println("Digite o segundo numero: ");
+        int numeroDois = terminal.nextInt();
+
+        try{
+    
+            contar(numeroUm, numeroDois);
+
+        } catch(ParametrosInvalidosException exception){
+
+            System.out.println("O segundo numero deve ser maior que o primeiro.");
+        }
+    }
+
+    static void contar(int numeroUm, int numeroDois) throws ParametrosInvalidosException{
+
+        if(numeroUm > numeroDois){
+            throw new ParametrosInvalidosException();
+        }
+
+        int contagem = numeroDois - numeroUm;
+
+        for(int i = 0; i < contagem; i++){
+            System.out.println(i);
+        }
+    }
+}
