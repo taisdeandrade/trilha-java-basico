565f838516c20c0a5ff5eb808ccc2df2077d3220
TaisAndrade <taismdeandrade@gmail.com>
2024-10-08 09:11:14 -0300

    fix: corrige o warning do scanner

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -28,5 +28,7 @@ public class ContaTerminal {
         nomeCliente = nome + " " + sobrenome;
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + "," + " conta " + numero + " e seu saldo de " + saldo + " já está disponível para saque.");
+
+        scanner.close();
     }
 }
